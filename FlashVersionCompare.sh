27b50e09737f8e82a2a058c52b514359f030d14f
Bryan Feuling <bfeuling@gmail.com>
2017-02-23 08:45:21 -0600

    Added Update Scripts and Comparison Scripts

---
+++ FlashVersionCompare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Get Latest Version
+LatestFlashVer=`/usr/bin/curl -s http://www.adobe.com/software/flash/about/ | sed -n '/Safari/,/<\/tr/s/[^>]*>\([0-9].*\)<.*/\1/p' | head -1`
+echo "Latest Flash Version is ${LatestFlashVer}"
+
+CurrFlashVer=`/usr/bin/defaults read "/Library/Internet Plug-Ins/Flash Player.plugin/Contents/version.plist" CFBundleVersion`
+echo "Current Flash Version is ${CurrFlashVer}"
+
+if [ "${LatestFlashVer}" != "${CurrFlashVer}" ]; then
+    echo "<result>UpdateRequired</result>"
+else
+    echo "<result>UpToDate</result>"
+fi
